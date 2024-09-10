dc2cc79fa9bc00137e5fc7b568d1397bc20c285b
Gassan Rashed Khalfan AL Htali <GRK816@KAHF.ORG>
2024-09-10 09:28:36 +0400

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webApi1.Data;
+using webApi1.Entities;
+
+namespace webApi1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly DataContext _context;
+
+        public UserController(DataContext context, ILogger<UserController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            var user = await _context.Users.FindAsync(id );
+            if (user == null) { return NotFound(); }
+            return user;
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApi1.Entities;
+
+namespace webApi1.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
